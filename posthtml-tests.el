4bc327a94e9aec0e3b3f40d983496a5fce01a913
Elo Laszlo <laszlo@manifold.io>
2017-08-12 23:18:28 -0400

    refactor aim and functionality
    build tool, instead of specific functionality

--- posthtml-tests.el
+++ posthtml-tests.el
@@ -10,98 +10,115 @@
   (org-export-define-derived-backend
       'test1 'html
     :filters-alist `((:filter-final-output
-                      ,(posthtml-filter-final-output
-                        (posthtml/doctype "THIS")))))
+                      ,(posthtml (setq contents "THIS")))))
   (should
-   (string-prefix-p "THIS\n<html"
-                    (with-temp-buffer (org-export-as 'test1 nil nil nil))))
+   (string= "THIS"
+            (with-temp-buffer (org-export-as 'test1 nil nil nil))))
 
   "default export process"
   (setf org-export-filter-final-output-functions nil)
-  (posthtml-add-export-filter (posthtml/doctype "THIS"))
+  (posthtml-filter-export-output (setq contents "THIS"))
   (should
-   (string-prefix-p "THIS\n<html"
-                    (with-temp-buffer (org-export-as 'html nil nil nil))))
+   (string= "THIS"
+            (with-temp-buffer (org-export-as 'html nil nil nil))))
 
   "org publishing process :preparation-action"
   (setf org-export-filter-final-output-functions nil)
-  (funcall (posthtml-add-filter (posthtml/doctype "THIS")))
+  (funcall (posthtml-add-export-filter (setq contents "THIS")))
   (should
-   (string-prefix-p "THIS\n<html"
-                    (with-temp-buffer (org-export-as 'html nil nil nil))))
+   (string= "THIS"
+            (with-temp-buffer (org-export-as 'html nil nil nil))))
 
   (setf org-export-filter-final-output-functions nil))
 
 
-(ert-deftest posthtml-doctype ()
+(ert-deftest posthtml/doctype ()
   "does not insert doctype if not already declared"
-  (let ((filter (posthtml (posthtml/doctype))))
-    (should (string-prefix-p
-             "<html"
-             (funcall filter "<html/>"))))
+  (let ((filter (posthtml (and t))))
+    (should
+     (string-prefix-p "<html"
+                      (funcall filter "<html/>"))))
 
-  "uses the INFO object to get current doctype"
-  (let ((filter (posthtml (posthtml/doctype))))
-    (should (string-prefix-p
-             "<!DOCTYPE html>"
-             (funcall filter "<!DOCTYPE something><html/>" '(:html-doctype "html5")))))
-
-  "doctype can be set with posthtml/doctype"
-  (let ((filter (posthtml (posthtml/doctype "html5"))))
-    (should (string-prefix-p
-             "<!DOCTYPE html>"
-             (funcall filter "<!DOCTYPE something><html/>"))))
-
-  "accepts custom doctype string"
-  (let ((filter (posthtml (posthtml/doctype "THIS"))))
-    (should (string-prefix-p
-             "THIS"
-             (funcall filter "<!DOCTYPE html><html/>")))))
+  "uses INFO to get current doctype"
+  (let ((filter (posthtml (and t))))
+    (should
+     (string-prefix-p "<!DOCTYPE html>"
+                      (funcall filter
+                               "<!DOCTYPE this><html/>"
+                               nil
+                               '(:html-doctype "html5"))))))
 
 
 (ert-deftest posthtml-special-characters ()
-  (let ((filter (posthtml (posthtml/doctype))))
-    (should (string= "<html><body>&amp; &gt; &lt;</body></html>"
-                     (funcall filter "<html><body>&amp; &gt; &lt;</body></html>")))
-    (should (string= "<html><body><!-- comment --></body></html>"
-             (funcall filter "<html><body><!-- comment --></body></html>")))))
+  (let ((filter (posthtml (and t))))
+    (should
+     (string= "<html><body>&amp; &gt; &lt;</body></html>"
+              (funcall filter "<html><body>&amp; &gt; &lt;</body></html>")))
+    (should
+     (string= "<html><body><!-- comment --></body></html>"
+              (funcall filter "<html><body><!-- comment --></body></html>")))))
 
 
-(ert-deftest posthtml$--nodes ()
-  "creates selector's dom nodes as required"
-  (should (string= (funcall (posthtml (posthtml$ [body p strong]))
-                            "<html/>")
-                   "<html><body><p><strong/></p></body></html>"))
-  (should (string= (funcall (posthtml (posthtml$ [body p strong]))
-                            "<html><body/></html>")
-                   "<html><body><p><strong/></p></body></html>"))
-  (should (string= (funcall (posthtml (posthtml$ [body p strong]))
-                            "<html><body><p/></body></html>")
-                   "<html><body><p><strong/></p></body></html>")))
-
-(ert-deftest posthtml$--args ()
-  "accepts contents (string) argument"
-  (should (string= (funcall (posthtml (posthtml$ [p strong] "hello"))
-                            "<html/>")
-                   "<html><p><strong>hello</strong></p></html>"))
-  "accepts contents (sxml) argument"
-  (should (string= (funcall (posthtml (posthtml$ [p] '(strong () "hello")))
-                            "<html/>")
-                   "<html><p><strong>hello</strong></p></html>")))
-
-(ert-deftest posthtml$--args:container-position ()
-  "accepts first as position argument"
-  (should (string= (funcall (posthtml (posthtml$ [head:first]))
-                            "<html><body/></html>")
-                   "<html><head/><body/></html>")))
+(ert-deftest posthtml-append ()
+  "function, accepting ELEMENT and ARGS"
+  (let ((filter (posthtml
+                 (posthtml$ contents [html] 'posthtml-append "this")
+                 ($ [html] :append "that"))))
+    (should
+     (string= "<html>thisthat</html>"
+              (funcall filter "<html/>")))))
 
 
-(ert-deftest posthtml/head-title ()
-  "adds html head title element"
-  (should (string= (funcall (posthtml (posthtml/head-title "hello"))
-                            "<html/>")
-                   "<html><head><title>hello</title></head></html>"))
-  "can be set to page #+TITLE: property"
-  (should (string= (funcall (posthtml (posthtml/head-title (posthtml: title)))
-                            "<html/>" '(:title "hello"))
-                   "<html><head><title>hello</title></head></html>")))
+(ert-deftest posthtml-prepend ()
+  "function, accepting ELEMENT and ARGS"
+  (let ((filter (posthtml
+                 (posthtml$ contents [html] 'posthtml-prepend "this")
+                 ($ [html] :prepend "that"))))
+    (should
+     (string= "<html>thatthis</html>"
+              (funcall filter "<html/>")))))
+
+
+(ert-deftest posthtml-attribute ()
+  (let ((filter (posthtml
+                 ($ [body] :attr 'class 'this)))
+        (input "<html><body/></html>")
+        (expected-result "<html><body class=\"this\"/></html>"))
+    (should
+     (string= expected-result (funcall filter input))))
+
+  (let ((filter (posthtml
+                 ($ [body] :attr 'class 'that)
+                 ($ [body] :attr 'id 'this)))
+        (input "<html><body class=\"this\"/></html>")
+        (expected-result "<html><body class=\"this that\" id=\"this\"/></html>"))
+    (should
+     (string= expected-result (funcall filter input)))))
+
+
+(ert-deftest posthtml-current-nav-elements ()
+  "example use - with lambda"
+  (let ((filter (posthtml
+                 ($each [nav li]
+                        (lambda (nav-element uri)
+                          (when (string-prefix-p uri (posthtml$ nav-element [a] :attr 'href))
+                            (posthtml-attribute nav-element 'class 'this)))
+                        "/this")))
+        (input (concat
+                "<html><body>"
+                "<nav><ul>"
+                "<li><a href=\"/not\">no</a></li>"
+                "<li><a href=\"/this\">yes</a></li>"
+                "<li><a href=\"/that\">no</a></li>"
+                "</ul></nav>"
+                "</body></html>"))
+        (expected-result (concat
+                          "<html><body>"
+                          "<nav><ul>"
+                          "<li><a href=\"/not\">no</a></li>"
+                          "<li class=\"this\"><a href=\"/this\">yes</a></li>"
+                          "<li><a href=\"/that\">no</a></li>"
+                          "</ul></nav>"
+                          "</body></html>")))
+    (should
+     (string= expected-result (funcall filter input)))))
--- posthtml.el
+++ posthtml.el
@@ -1,12 +1,12 @@
 ;;; posthtml.el --- post HTML rendering filters for org-export
 ;; Copyright (C) Elo Laszlo 2017
 
-;; Author: Elo Laszlo <laszlo at manifold dot io>
+;; Author: Elo Laszlo <hello at bald dot cat>
 ;; Created: August 2016
-;; Updated: Feb 2017
+;; Updated: Aug 2017
 ;; Description: post HTML rendering filters for org-export
-;; Homepage: http://manifold.io/project/posthtml
-;; Version: 0.2.0
+;; Homepage: http://bald.cat/project/posthtml
+;; Version: 0.3.0
 ;; Package-Requires: ((esxml "20160703.1417")(enlive "20150824.549"))
 ;;
 ;; This program is free software; you can redistribute it and/or modify
@@ -23,145 +23,181 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
-;; posthtml defines post rendering hooks for org-export;
-;; it works with a parse-tree representation of the current page,
-;; providing programmatic access to it's DOM.
-;;
-;; (posthtml (posthtml/doctype "<!DOCTYPE html>")
-;;           (posthtml/head-title (posthtml: title))
-;;           (posthtml$ [html body article:first] "add text")
-;;           [...])
 
 ;;; Code:
-(require 'esxml)
-(require 'enlive)
+(require 'esxml)                        ; esxml-to-xml
+(require 'enlive)                       ; enlive-query, enlive-query-all
+(eval-when-compile (require 'cl))       ; lexical-let, cl-macrolet
+(require 'ox-html)                      ; org-html-doctype
+
+
+;;;###autoload
+(defmacro posthtml (&rest body)
+  "An org-export-filter-final-output-functions definition with
+the contents of BODY. CONTENTS and INFO are lexically bound.
+
+CONTENTS is an esxml representation of the DOM tree, as parsed
+by libxml-parse-html-region; the function returns a string via
+esxml-to-xml.
+
+INFO is the org-export property plist. The doctype, when
+specified, is derived from INFO.
+
+Within BODY, the following shorthand is available;
+=($ [selector] &optional fn args)= for
+=(posthtml$ contents [selector] &optional fn args)=,
+=($each [selector] &optional fn args)= for
+=(posthtml$each contents [selector] &optional fn args)=."
+  (let ((contents (make-symbol "contents"))
+        (backend (make-symbol "backend"))
+        (info (make-symbol "info")))
+    `(function
+      (lambda (,contents &optional ,backend ,info)
+        (posthtml-render
+         (lexical-let
+             ((contents (if (not (listp ,contents))
+                            (posthtml-parse-html ,contents)
+                          ,contents))
+              (info ,info))
+           ;; be aware of macro binding when fn is a lambda
+           (cl-macrolet
+               (($ (selector &optional fn &rest args)
+                   `(funcall 'posthtml$ contents ,selector ,fn ,@args))
+                ($each (selector &optional fn &rest args)
+                       `(funcall 'posthtml$each contents ,selector ,fn ,@args)))
+             ,@body)
+           contents)
+         (and (and (not (listp ,contents)) ,info)
+              (string-prefix-p "<!DOCTYPE" ,contents t)
+              (org-html-doctype ,info)))))))
+
+
+;;;###autoload
+(defmacro posthtml-filter-export-output (&rest body)
+  "add a posthtml filter to the default export process"
+  `(add-to-list 'org-export-filter-final-output-functions (posthtml ,@body)))
+
+;;;###autoload
+(defmacro posthtml-add-export-filter (&rest body)
+  "add a posthtml filter to the default export process,
+returns an org export filter lambda"
+  `(lambda (&optional project-properties) (posthtml-filter-export-output ,@body)))
+
+
+(defun posthtml-render (contents &optional doctype)
+  "return DOCTYPE and CONTENTS as a string"
+  (format "%s%s"
+          (if doctype (concat doctype "\n") "")
+          (if (listp contents)
+              (posthtml-parse-esxml contents)
+            contents)))
 
 (defvar posthtml-special-chars '("amp" "gt" "lt"))
 
+(defun posthtml-parse-html (contents)
+  (with-current-buffer (get-buffer-create " *posthtml*")
+    (erase-buffer)
+    (insert contents)
+    (loop for c in posthtml-special-chars do
+          (goto-char 1)
+          (while (search-forward (concat "&" c ";") nil t)
+            (replace-match (concat "%%" c "%%") nil t)))
+    (libxml-parse-html-region (point-min) (point-max))))
 
-(defmacro posthtml-filter-final-output (&rest body)
-  "an org-export-filter-final-output-functions
-definition; a posthtml filter is defined with the
-contents of BODY"
-  `(lambda (contents backend info)
-     (funcall (posthtml ,@body) contents info)))
-
-(defmacro posthtml-add-export-filter (&rest body)
-  "add a posthtml filter to the default export process;
-a posthtml filter is defined with the contents of BODY"
-  `(add-to-list 'org-export-filter-final-output-functions
-                (posthtml-filter-final-output ,@body)))
-
-(defmacro posthtml-add-filter (&rest body)
-  "return a posthtml filter lambda; a posthtml filter
-is defined with the contents of BODY"
-  `(lambda (&optional project-properties)
-     (posthtml-add-export-filter ,@body)))
-
-
-(defmacro posthtml (&rest body)
-  `(lambda (contents &optional info)
-     (let ((doctype (and (not (listp contents))
-                         (string-prefix-p "<!DOCTYPE" contents t)
-                         (plist-get info :html-doctype))))
-       (posthtml-parse-html)
-       ,@body
-       (posthtml-parse-esxml)
-       (if doctype
-           (format "%s\n%s"
-                   (or (cdr (assoc doctype org-html-doctype-alist)) doctype)
-                   contents)
-         (format "%s" contents)))))
+(defun posthtml-parse-esxml (contents)
+  (with-current-buffer (get-buffer-create " *posthtml*")
+    (erase-buffer)
+    (insert (esxml-to-xml contents))
+    (loop for c in posthtml-special-chars do
+          (goto-char 1)
+          (while (search-forward (concat "%%" c "%%") nil t)
+            (replace-match (concat "&" c ";") nil t)))
+    (goto-char 1)
+    (while (search-forward "<comment>" nil t) (replace-match "<!--" nil t))
+    (goto-char 1)
+    (while (search-forward "</comment>" nil t) (replace-match "-->" nil t))
+    (buffer-string)))
 
 
-(defun posthtml-parse-html ()
-  (when (not (listp contents))
-    (setf contents
-          (with-current-buffer (get-buffer-create " *posthtml*")
-            (erase-buffer)
-            (insert contents)
-            (loop for c in posthtml-special-chars do
-                  (goto-char 1)
-                  (while (search-forward (concat "&" c ";") nil t)
-                    (replace-match (concat "%%" c "%%") nil t)))
-            (libxml-parse-html-region (point-min) (point-max))))))
+(defvar posthtml-fn-tokens
+  '(:append posthtml-append :prepend posthtml-prepend :attr posthtml-attribute))
 
-(defun posthtml-parse-esxml ()
-  (when (listp contents)
-    (setf contents
-          (with-current-buffer (get-buffer-create " *posthtml*")
-            (erase-buffer)
-            (insert (esxml-to-xml contents))
-            (loop for c in posthtml-special-chars do
-                  (goto-char 1)
-                  (while (search-forward (concat "%%" c "%%") nil t)
-                    (replace-match (concat "&" c ";") nil t)))
-            (goto-char 1)
-            (while (search-forward "<comment>" nil t) (replace-match "<!--" nil t))
-            (goto-char 1)
-            (while (search-forward "</comment>" nil t) (replace-match "-->" nil t))
-            (buffer-string)))))
+(defun posthtml$each (container selector &optional fn &rest args)
+  "Query CONTAINER for SELECTOR; apply each found element and
+ARGS to FN, return results.
 
+CONTAINER is an esxml list, SELECTOR is a vector; ie [html body].
 
-(defun posthtml-append (container element)
-  (if (= 1 (length container))
-      (nconc container (list '() element))
-    (nconc container (list element)))
-  container)
+The following shorthands are available for FN;
+:append for 'posthtml-append
+:prepend for 'posthtml-prepend
+:attr for 'posthtml-attribute"
+  (if fn
+      (setq fn (or (plist-get posthtml-fn-tokens fn) fn))
+    (setq fn 'identity))
+  (mapcar (lambda (element) (apply fn element args))
+          (enlive-query-all container selector)))
 
-(defun posthtml-prepend (container element)
-  (if (> 3 (length container))
-      (posthtml-append container element)
-    (setf (nthcdr 2 container) (append (list element) (nthcdr 2 container))))
-  container)
+(defun posthtml$ (container selector &optional fn &rest args)
+  "Query CONTAINER for SELECTOR; apply found element and ARGS
+to FN, return results.
+
+CONTAINER is an esxml list, SELECTOR is a vector; ie [html body].
+
+The following shorthands are available for FN;
+:append for 'posthtml-append
+:prepend for 'posthtml-prepend
+:attr for 'posthtml-attribute"
+  (if fn
+      (setq fn (or (plist-get posthtml-fn-tokens fn) fn))
+    (setq fn 'identity))
+  (apply fn (enlive-query container selector) args))
 
 
-(defun posthtml-find (elements &optional selector)
-  (when selector
-    (or (enlive-query elements selector)
-        (let* ((selector (append selector nil))
-               (prepend nil)
-               (element (let ((element (split-string (format "%s" (car selector)) ":" t)))
-                          (when (< 1 (length element))
-                            (cond ((string= "first" (cadr element)) (setf prepend t))))
-                          (intern (car element))))
-               (container (enlive-query elements (vconcat (list element)))))
-          (if container (or (posthtml-find container (vconcat (cdr selector))) container)
-            (posthtml-find (if prepend (posthtml-prepend elements (list element))
-                             (posthtml-append elements (list element)))
-                           (vconcat selector)))))))
+(defun posthtml-append (container &optional element)
+  "Append ELEMENT to CONTAINER.
 
-(defun posthtml$ (selector &optional content)
-  (let ((element (posthtml-find contents selector)))
-    (when (and element content) (posthtml-append element content))
-    element))
+CONTAINER is an esxml list, ELEMENT is a list or a string."
+  (when element
+    (if (= 1 (length container))
+        (nconc container (list '() element))
+      (nconc container (list element)))))
 
+(defun posthtml-prepend (container &optional element)
+  "Prepend ELEMENT to CONTAINER.
 
-(defmacro def-posthtml/element (selector)
-  (let ((name (mapconcat (lambda (s)
-                           (let ((s (format "%s" s))) (subseq s 0 (search ":" s))))
-                         (cdr (append selector nil)) "-")))
-    `(defun ,(intern (concat "posthtml/" name)) (&optional content)
-       (posthtml$ ,selector content))))
+CONTAINER is an esxml list, ELEMENT is a list or a string."
+  (when element
+    (if (> 3 (length container))
+        (posthtml-append container element)
+      (setf (nthcdr 2 container)
+            (append (list element) (nthcdr 2 container))))
+    container))
 
-(defun posthtml/doctype (&optional flavour)
-  "set the posthtml filter's doctype property
-to FLAVOUR, overriding what might have been
-defined in filter's INFO originally."
-  (when flavour (setf doctype flavour)))
+(defun posthtml-attribute (element attribute &optional values)
+  "Return ELEMENT ATTRIBUTE; with VALUES argument, add ATTRIBUTE
+with VALUE to ELEMENT.
 
-
-(def-posthtml/element [html head:first title])
-
-
-(defmacro posthtml: (key)
-  (let ((key (intern (concat ":" (format "%s" key)))))
-    `(let ((value (plist-get info ,key)))
-       ,(if (member key '(:title :author))
-            '(if (and value (listp value)) (substring-no-properties (car value))
-               (or value ""))
-          '(or value "")))))
+ELEMENT is an esxml list, ATTRIBUTE and VALUES are strings."
+  (let ((attributes (nth 1 element)))
+    (when values
+      (let ((new-attribute (make-symbol (format "%s" attribute)))
+            (value (format "%s" values)))
+        (cond ((null attributes)
+               (setf (nth 1 element)
+                     (list (cons new-attribute value))))
+              ((listp attributes)
+               (let ((current-attribute (assoc attribute (nth 1 element))))
+                 (if current-attribute
+                     (setcdr current-attribute
+                             (format "%s %s" (cdr current-attribute) value))
+                   (setf (nth 1 element)
+                         (nconc (nth 1 element)
+                                (list (cons new-attribute value))))))))))
+    (and (not (null attributes))
+         (listp attributes)
+         (cdr (assoc attribute (nth 1 element))))))
 
 
 (provide 'posthtml)
+;;; posthtml.el ends here
